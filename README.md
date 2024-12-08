9804ec5ecce1283479d3f37228f15a524c8b34f4
ecl <yozh.asmodeus@gmail.com>
2024-12-08 20:45:11 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Train airflow skills
+# data set https://www.kaggle.com/datasets/asaniczka/1-3m-linkedin-jobs-and-skills-2024
---
+++ linkedin_dags/extract_raw_data.py
@@ -0,0 +1,60 @@
+import json
+
+import pendulum
+
+from airflow.decorators import dag, task
+
+@dag(
+    dag_id="extract_csv_data",
+    schedule=None,
+    start_date=pendulum.datetime(2021, 1, 1, tz="UTC"),
+    catchup=False,
+    tags=["extract"],
+)
+
+def extract_job_skils_taskflow_api():
+    @task()
+    def import_csv_to_db():
+        import pandas as pd
+        import boto3
+        from botocore.exceptions import ClientError
+
+        csv_path = '/home/vagrant/job_skills.csv'
+
+        client = boto3.client(
+            's3',
+            aws_access_key_id='accessKey1',
+            aws_secret_access_key='verySecretKey1',
+            endpoint_url='http://127.0.0.1:8000'
+        )
+
+        client.create_bucket(Bucket='csv_raw_chunks')
+
+        def upload_s3_file(file_name, bucket, object_name=None):
+            if object_name is None:
+                object_name = os.path.basename(file_name)
+
+            # Upload the file
+            s3_client = boto3.client('s3')
+            try:
+                response = client.upload_file(file_name, bucket, object_name)
+            except ClientError as e:
+                print(e)
+                return False
+            return True
+
+
+        
+        chunksize = 100
+        batch_no = 1
+        for chunk in pd.read_csv(csv_path, chunksize=chunksize):
+            csv_batch_name = 'job_skills_chunk_{}.csv'.format(batch_no)
+            chunk.to_csv(csv_batch_name, index=False)
+            upload_s3_file(csv_batch_name, bucket='csv_raw_chunks')
+            batch_no +=1
+
+        
+
+    job_skills = import_csv_to_db()
+
+extract_job_skils_taskflow_api()
