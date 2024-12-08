f178fa41955501c6d0b6ceec7bc011a7be7d409f
ecl <yozh.asmodeus@gmail.com>
2024-12-09 00:44:36 +0200

    maybe its fix idk

--- linkedin_dags/clear_s3_buckets.py
+++ linkedin_dags/clear_s3_buckets.py
@@ -6,7 +6,7 @@ import pendulum
     schedule=None,
     start_date=pendulum.datetime(2021, 1, 1, tz="UTC"),
     catchup=False,
-    tags=["services"],
+    tags=["extract"]
 )
 def clear_s3():
     @task()
