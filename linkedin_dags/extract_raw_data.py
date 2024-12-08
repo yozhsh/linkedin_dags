4ae4b13914f54566f278be4b115c7d7e180605c4
ecl <yozh.asmodeus@gmail.com>
2024-12-09 01:57:13 +0200

    try skip bad lines

--- linkedin_dags/extract_raw_data.py
+++ linkedin_dags/extract_raw_data.py
@@ -138,12 +138,15 @@ def extract_linkedin_ds_s3():
         chunksize = 100
         batch_no = 1
 
-        for chunk in pd.read_csv(csv_path, chunksize=chunksize, quoting=csv.QUOTE_NONE):
-            csv_batch_name = 'jobsummary_chunk_{}.csv'.format(batch_no)
-            chunk.to_csv(csv_batch_name, index=False)
-            upload_s3_file(csv_batch_name, bucket_name)
-            os.remove('/home/vagrant/airflow/jobsummary_chunk_{}.csv'.format(batch_no))
-            batch_no +=1
+        for chunk in pd.read_csv(csv_path, chunksize=chunksize):
+            try:
+                csv_batch_name = 'jobsummary_chunk_{}.csv'.format(batch_no)
+                chunk.to_csv(csv_batch_name, index=False)
+                upload_s3_file(csv_batch_name, bucket_name)
+                os.remove('/home/vagrant/airflow/jobsummary_chunk_{}.csv'.format(batch_no))
+            except pd.errors.ParserError:
+                batch_no +=1
+                continue
         
 
     import_jobskills_chunks_to_s3()
