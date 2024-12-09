3967e41129408e3c331c4ac4b46ec8f389c3979c
ecl <yozh.asmodeus@gmail.com>
2024-12-09 19:35:05 +0200

    in progress

--- linkedin_dags/linkedin_ETL.py
+++ linkedin_dags/linkedin_ETL.py
@@ -1,5 +1,4 @@
 import json
-import sys
 
 import pendulum
 import os
@@ -8,7 +7,33 @@ from airflow.decorators import dag, task
 from airflow.providers.common.sql.operators.sql import SQLExecuteQueryOperator
 
 
-sys.path.append('linkedin_dags/utils')
+
+def s3client():
+    import boto3
+
+    client = boto3.client(
+            's3',
+            aws_access_key_id='accessKey1',
+            aws_secret_access_key='verySecretKey1',
+            endpoint_url='http://10.0.0.30:8000/'
+        )
+    return client
+
+def upload_s3_file(s3client, file_name, bucket, object_name=None):
+            import os
+            from boto3.exceptions import ClientError
+
+            if object_name is None:
+                object_name = os.path.basename(file_name)
+
+            # Upload the file
+            try:
+                response = s3client.upload_file(file_name, bucket, object_name)
+            except ClientError as e:
+                print(e)
+                return False
+            return True
+
 
 @dag(
     dag_id="LinkedinETL",
@@ -163,7 +188,6 @@ def etl():
 
     @task()
     def transform_jobskills_chunk_to_db():
-        from utils.s3 import s3client
         import pandas as pd
 
         s3 = s3client()
