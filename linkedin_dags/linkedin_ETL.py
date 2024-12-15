63203c98510eabb80ab368fb68e9dc4b288eb874
ecl <yozh.asmodeus@gmail.com>
2024-12-15 17:34:21 +0200

    jobskill extract

--- linkedin_dags/linkedin_ETL.py
+++ linkedin_dags/linkedin_ETL.py
@@ -447,8 +447,8 @@ def etl():
             bucket.download_file(filename, download_path)
             df = pd.read_csv(download_path)
             joblink = df.get('job_link')
-            dicted = joblink.to_dict()
-            print(dicted)
+            links = joblink.to_list()
+            print(links)
             break
         
         cursor.close()
